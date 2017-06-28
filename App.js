73fe7101d45e76a852756eaae1513c1a9cb705ae
Owen Auch <owenauch@wustl.edu>
2017-06-28 15:51:36 -0400

    first shell

--- App.js
+++ App.js
@@ -1,23 +1,73 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native'
+
+import { Avatar, Button } from 'react-native-elements'
 
 export default class App extends React.Component {
   render() {
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={styles.mainContainer}>
+        <Avatar
+          containerStyle={styles.cardButton}
+          source={require('./icons/ace.png')}
+          large
+          rounded
+        />
+        <Avatar
+          containerStyle={styles.cardButton}
+          source={require('./icons/king.png')}
+          large
+          rounded
+        />
+        <Avatar
+          containerStyle={styles.cardButton}
+          source={require('./icons/queen.png')}
+          large
+          rounded
+        />
+        <Avatar
+          containerStyle={styles.cardButton}
+          source={require('./icons/jack.png')}
+          large
+          rounded
+        />
+        <Avatar
+          containerStyle={styles.cardButton}
+          source={require('./icons/10.png')}
+          large
+          rounded
+        />
+        <Avatar
+          containerStyle={styles.cardButton}
+          source={require('./icons/9.png')}
+          large
+          rounded
+        />
+        <Button
+          large
+          title='Submit Hand'
+          borderRadius={20}
+          backgroundColor={'red'}
+          containerViewStyle={styles.submitHandButton}
+        />
       </View>
-    );
+    )
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'green',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   },
-});
+
+  cardButton: {
+  },
+
+  submitHandButton: {
+    width: 200
+  }
+})
